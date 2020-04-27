b55ed64cdebc38666dde04cde21d93729965111e
blazinskip <blazinski.patryk@gmail.com>
2020-04-27 18:04:56 +0200

    feat: 🎸 [ts] run-length-encoding decode v 1.0

--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -5,7 +5,7 @@ class RunLengthEncoding {
     let result = '';
     for (let i = 0; i < characters.length; i++) {
       let count = 1;
-      while (i < characters.length -1 && characters[i] === characters[i + 1]) {
+      while (i < characters.length - 1 && characters[i] === characters[i + 1]) {
         count++;
         i++;
       }
@@ -16,8 +16,32 @@ class RunLengthEncoding {
     return result;
   }
 
-  static decode(_textToDecode: string): string {
-    return '';
+  static decode(textToDecode: string): string {
+    const characters = [...textToDecode];
+
+    let result = '';
+    for (let i = 0; i < characters.length; i++) {
+      let currentCharacter = characters[i];
+
+      let numberFromCurrentCharacter = Number(currentCharacter); // Number(' ') === 0
+      let occurrenceCounterString = '';
+      while (i < characters.length - 1 && !isNaN(numberFromCurrentCharacter) && numberFromCurrentCharacter !== 0) {
+        occurrenceCounterString = `${occurrenceCounterString}${numberFromCurrentCharacter}`;
+        i++;
+        currentCharacter = characters[i];
+        numberFromCurrentCharacter = Number(currentCharacter);
+      }
+
+      const counter = Number(occurrenceCounterString ? occurrenceCounterString : 1);
+
+      const joined = Array.from(Array(counter))
+        .reduce((acc, _current) => `${acc}${currentCharacter}`, '');
+
+      result = `${result}${joined}`;
+    }
+
+
+    return result;
   }
 }
 
