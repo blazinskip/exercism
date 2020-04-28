0a95ebefb3ab0906144b45dab0d8f129d039ad59
blazinskip <blazinski.patryk@gmail.com>
2020-04-28 16:16:54 +0200

    feat: 🎸 [ts] triangle solution v 1.0

--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -1,12 +1,32 @@
 export default class Triangle {
+  private readonly aSide: number;
+  private readonly bSide: number;
+  private readonly cSide: number;
 
-    sides: number[]
+  constructor(...sides: number[]) {
+    const [a, b, c] = sides;
+    this.aSide = a;
+    this.bSide = b;
+    this.cSide = c;
+  }
 
-    constructor(...sides: number[]) {
-        this.sides = sides
+  kind() {
+    if (Triangle.isNotTriangle(this.aSide, this.bSide, this.cSide)) {
+      throw new Error('is not a triangle');
     }
 
-    kind() {
-        return ""
+    if (this.aSide === this.bSide && this.bSide === this.cSide) {
+      return 'equilateral';
     }
+
+    if (this.aSide === this.bSide || this.aSide === this.cSide || this.bSide === this.cSide) {
+      return 'isosceles';
+    }
+
+    return "scalene";
+  }
+
+  private static isNotTriangle(a: number, b: number, c: number) {
+    return a + b <= c || a + c <= b || b + c <= a;
+  }
 }
