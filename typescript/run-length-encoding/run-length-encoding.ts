363cc08b0739c5b84dc5b8fddce71fbc32168da6
blazinskip <blazinski.patryk@gmail.com>
2020-04-28 16:01:16 +0200

    feat: 🎸 [ts] init triangle

--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -1,19 +1,6 @@
 class RunLengthEncoding {
   static encode(textToEncode: string): string {
-    const characters = [...textToEncode];
-
-    let result = '';
-    for (let i = 0; i < characters.length; i++) {
-      let count = 1;
-      while (i < characters.length - 1 && characters[i] === characters[i + 1]) {
-        count++;
-        i++;
-      }
-
-      result = `${result}${count > 1 ? count : ''}${characters[i]}`
-    }
-
-    return result;
+    return textToEncode.replace(/(.)\1+/g, substring => substring.length + substring[0]);
   }
 
   static decode(textToDecode: string): string {
