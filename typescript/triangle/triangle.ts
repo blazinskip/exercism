922503c7469da065a9eb3261cc9a846798ee08b8
blazinskip <blazinski.patryk@gmail.com>
2020-04-28 16:26:04 +0200

    feat: 🎸 [ts] triangle v 3.0

--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -1,27 +1,24 @@
 type TriangleKind = 'equilateral' | 'isosceles' | 'scalene';
 
 export default class Triangle {
-  private readonly aSide: number;
-  private readonly bSide: number;
-  private readonly cSide: number;
+  private readonly sortedSides: number[];
 
   constructor(...sides: number[]) {
-    const [a, b, c] = sides;
-    this.aSide = a;
-    this.bSide = b;
-    this.cSide = c;
+    this.sortedSides = sides.sort((a, b) => a - b);
   }
 
   kind(): TriangleKind {
-    if (Triangle.isNotTriangle(this.aSide, this.bSide, this.cSide)) {
-      throw new Error('is not a triangle');
+    const [a, b, c] = this.sortedSides;
+
+    if (a + b <= c) {
+      throw new Error('Illegal triangle');
     }
 
-    if (this.aSide === this.bSide && this.bSide === this.cSide) {
+    if (a === c) {
       return 'equilateral';
     }
 
-    if (this.aSide === this.bSide || this.aSide === this.cSide || this.bSide === this.cSide) {
+    if (a === b || b === c) {
       return 'isosceles';
     }
 
