37c76fb2a2ce134c8c452acd29bd786ba04af461
blazinskip <blazinski.patryk@gmail.com>
2020-04-30 17:55:20 +0200

    feat: 🎸 [ts] acronym v 2.0

--- typescript/acronym/acronym.ts
+++ typescript/acronym/acronym.ts
@@ -1,9 +1,5 @@
 export default class Acronym {
   public static parse(phrase: string): string {
-    return phrase
-      .replace(/([a-z])([A-Z])/g, '$1 $2')
-      .split(/[ -]+/)
-      .map(word => word[0].toUpperCase())
-      .join('');
+    return phrase.replace(/(\w)[A-Z]*[a-z]*\W*/g, "$1").toUpperCase();
   }
 }
