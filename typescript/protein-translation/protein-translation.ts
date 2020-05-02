f296f6df73717596aed45baaa6e42f40208a8966
blazinskip <blazinski.patryk@gmail.com>
2020-05-02 08:29:10 +0200

    feat: 🎸 [ts] protein-translation v 1.0

--- typescript/protein-translation/protein-translation.ts
+++ typescript/protein-translation/protein-translation.ts
@@ -1,7 +1,67 @@
+type Proteins = 'Methionine'
+  | 'Phenylalanine'
+  | 'Leucine'
+  | 'Serine'
+  | 'Tyrosine'
+  | 'Cysteine'
+  | 'Tryptophan';
+
+type Codons = 'AUG'
+  | 'UUU'
+  | 'UUC'
+  | 'UUA'
+  | 'UUG'
+  | 'UCU'
+  | 'UCC'
+  | 'UCA'
+  | 'UCG'
+  | 'UAU'
+  | 'UAC'
+  | 'UGU'
+  | 'UGC'
+  | 'UGG'
+  | 'UAA'
+  | 'UAG'
+  | 'UGA';
+
+type TerminatingCommand = 'STOP';
+
 class ProteinTranslation {
-    static proteins(rnaSequence: string): string[] {
-        return [];
+  private static codonsToProtein: { [key in Codons]: Proteins | TerminatingCommand } = {
+    AUG: 'Methionine',
+    UUU: 'Phenylalanine',
+    UUC: 'Phenylalanine',
+    UUA: 'Leucine',
+    UUG: 'Leucine',
+    UCU: 'Serine',
+    UCC: 'Serine',
+    UCA: 'Serine',
+    UCG: 'Serine',
+    UAU: 'Tyrosine',
+    UAC: 'Tyrosine',
+    UGU: 'Cysteine',
+    UGC: 'Cysteine',
+    UGG: 'Tryptophan',
+    UAA: 'STOP',
+    UAG: 'STOP',
+    UGA: 'STOP',
+  }
+
+  static proteins(rnaSequence: string): Proteins[] {
+    const codons = (rnaSequence.match(/.{1,3}/g) || []) as Codons[];
+    const result: Proteins[] = [];
+
+    for (let codon of codons) {
+      if (ProteinTranslation.codonsToProtein[codon] === 'STOP') {
+        break;
+      }
+
+      result.push(ProteinTranslation.codonsToProtein[codon] as Proteins);
     }
+
+    return result;
+  }
 }
 
 export default ProteinTranslation
+
