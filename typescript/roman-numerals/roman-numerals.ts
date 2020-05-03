fc044406c4c256565a849f6e4f803b55d558923d
blazinskip <blazinski.patryk@gmail.com>
2020-05-03 08:11:41 +0200

    feat: 🎸 [ts] init roman numerals

---
+++ typescript/roman-numerals/roman-numerals.ts
@@ -0,0 +1,11 @@
+type RomanNumber = 'I' | 'V' | 'X' | 'L' | 'C' | 'D' | 'M';
+
+class RomanNumerals {
+  private static 
+
+  static roman(number: number): string {
+    // Your code here
+  }
+}
+
+export default RomanNumerals
