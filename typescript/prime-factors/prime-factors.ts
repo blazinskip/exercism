74cda160041dfdc31b012b64b296b8a730fd7942
blazinskip <blazinski.patryk@gmail.com>
2020-05-05 15:52:56 +0200

    feat: 🎸 [ts] prime-factors v 1.0

--- typescript/prime-factors/prime-factors.ts
+++ typescript/prime-factors/prime-factors.ts
@@ -1,3 +1,21 @@
 export default function calculatePrimeFactors(number: number): number[] {
-  return [];
+  const primeFactors = [];
+  while (number % 2 === 0) {
+    primeFactors.push(2);
+    number = number / 2;
+  }
+
+  const sqrtNum = Math.sqrt(number);
+  for (let i = 3; i <= sqrtNum; i++) {
+    while (number % i === 0) {
+      primeFactors.push(i);
+      number = number / i;
+    }
+  }
+
+  if (number > 2) {
+    primeFactors.push(number);
+  }
+
+  return primeFactors;
 };
