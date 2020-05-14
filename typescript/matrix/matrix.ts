e8d0f23e75f5f74e7870354bc000c67fcf23c45d
blazinskip <blazinski.patryk@gmail.com>
2020-05-14 16:21:50 +0200

    feat: 🎸 [ts] matrix v 1.0

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,7 +1,26 @@
 class Matrix {
-    constructor( /* Parameters go here */ ) {
-        // Your code here
-    }
+  private readonly _rows: number[][];
+  private readonly _columns: number[][] = [[]];
+
+  constructor(matrix: string) {
+    const rows = matrix.split('\n');
+    this._rows = rows.map(row => {
+      return row.split(' ')
+        .map(Number);
+    });
+
+    this._columns = this._rows.reduce((prev, next) => next.map((_item, index) =>
+      (prev[index] || [] as number[]).concat(next[index])
+    ), [[]] as number[][]);
+  }
+
+  get rows(): number[][] {
+    return [...this._rows];
+  }
+
+  get columns(): number[][] {
+    return [...this._columns];
+  }
 }
 
 export default Matrix
