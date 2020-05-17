39d22c95ea37ee7eff66ea6cca7317244e1128fd
blazinskip <blazinski.patryk@gmail.com>
2020-05-17 12:10:15 +0200

    feat: 🎸 [ts] isbn-verifier v 1.1

--- typescript/isbn-verifier/isbn-verifier.ts
+++ typescript/isbn-verifier/isbn-verifier.ts
@@ -2,36 +2,39 @@ export default class Isbn {
   private readonly valid: boolean = true;
 
   constructor(isbn: string) {
-    const isbnWithoutHyphens = isbn.replace(/-/g, "");
+    const normalizedNumber = Isbn.normalizeNumber(isbn);
 
-    const xIndexOf = isbnWithoutHyphens.indexOf('X');
-
-    if (xIndexOf !== -1 && xIndexOf !== isbnWithoutHyphens.length - 1) {
+    if (Isbn.isValidNumber(normalizedNumber)) {
+      const checksum = Isbn.calculateChecksum(normalizedNumber);
+      this.valid = checksum % 11 === 0;
+    } else {
       this.valid = false;
-      return;
     }
-
-    if (!isbnWithoutHyphens.match("^[X0-9]+$")) {
-      this.valid = false;
-      return;
-    }
-
-    let multiplier = 10;
-    const sum = [...isbnWithoutHyphens].reduce((sum, currentNumber) => {
-      const number = Number(currentNumber);
-      if (!isNaN(number)) {
-        sum = sum + (multiplier * number);
-      } else {
-        sum = sum + (multiplier * 10);
-      }
-      multiplier--;
-      return sum;
-    }, 0);
-
-    this.valid = sum % 11 === 0;
   }
 
   isValid() {
     return this.valid;
+  }
+
+  private static calculateChecksum(normalizedNumber: string): number {
+    let multiplier = 10;
+    return [...normalizedNumber].reduce((sum, digit) => {
+      const value = Isbn.digitToValue(digit);
+      sum += value * multiplier;
+      multiplier--;
+      return sum;
+    }, 0);
+  }
+
+  private static digitToValue(digit: string): number {
+    return digit === 'X' ? 10 : parseInt(digit, 10);
+  }
+
+  private static normalizeNumber(isbn: string): string {
+    return isbn.replace(/-/g, "");
+  }
+
+  private static isValidNumber(normalizedNumber: string): boolean {
+    return /^\d{9}[\dX]$/.test(normalizedNumber);
   }
 }
