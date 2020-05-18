a8b8acff94b7f6e02beb95b2d118744d4ca11b28
blazinskip <blazinski.patryk@gmail.com>
2020-05-18 16:22:59 +0200

    feat: 🎸 [ts] secret-handshake v 1.1

--- typescript/secret-handshake/secret-handshake.ts
+++ typescript/secret-handshake/secret-handshake.ts
@@ -1,39 +1,22 @@
 export default class SecretHandshake {
+  private static readonly actionsMap: Map<number, string> = new Map([
+    [0b1, "wink"],
+    [0b10, "double blink"],
+    [0b100, "close your eyes"],
+    [0b1000, "jump"]
+  ]);
+
   private readonly handshakeCommands: string[] = [];
 
   constructor(private readonly numberInDecimal: number) {
-    let binary = SecretHandshake.toBinaryNumber(numberInDecimal);
+    const commands = [...SecretHandshake.actionsMap]
+      .filter(([command]) => this.numberInDecimal & command)
+      .reduce<string[]>((result, [, cmd]) => [...result, cmd], []);
 
-    if (binary % 10 === 1) {
-      binary = binary - 1;
-      this.handshakeCommands.push('wink');
-    }
-
-    if (binary % 100 === 10) {
-      binary = binary - 10;
-      this.handshakeCommands.push('double blink');
-    }
-
-    if (binary % 1000 === 100) {
-      binary = binary - 100;
-      this.handshakeCommands.push('close your eyes');
-    }
-
-    if (binary % 10000 === 1000) {
-      binary = binary - 1000;
-      this.handshakeCommands.push('jump');
-    }
-
-    if (binary % 100000 === 10000) {
-      this.handshakeCommands = this.handshakeCommands.reverse();
-    }
+    this.handshakeCommands = this.numberInDecimal & 0b10000 ? commands.reverse() : commands;
   }
 
   commands() {
     return this.handshakeCommands;
-  }
-
-  private static toBinaryNumber(numberInDecimal: number): number {
-    return Number(numberInDecimal.toString(2));
   }
 }
