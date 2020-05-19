03fdd2eb31a3016a8bc94058c45a4a0d7e0af3a2
blazinskip <blazinski.patryk@gmail.com>
2020-05-19 15:44:07 +0200

    feat: 🎸 [ts] robot-name v 1.0

---
+++ typescript/robot-name/robot-name.ts
@@ -0,0 +1,42 @@
+export default class RobotName {
+  private static currentNames: string[] = [];
+  private static letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  private static digits = '0123456789';
+
+  private _name = '';
+
+  constructor() {
+    this._name = RobotName.generateName();
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  resetName(): void {
+    this._name = RobotName.generateName();
+  }
+
+  private static generateName(): string {
+    let name = [
+      ...RobotName.generateNthRandomCharactersFromString(2, RobotName.letters),
+      ...RobotName.generateNthRandomCharactersFromString(3, RobotName.digits)
+    ].join('');
+
+    while (RobotName.currentNames.includes(name)) {
+      name = [
+        ...RobotName.generateNthRandomCharactersFromString(2, RobotName.letters),
+        ...RobotName.generateNthRandomCharactersFromString(3, RobotName.digits)
+      ].join('');
+    }
+
+    this.currentNames = [...this.currentNames, name];
+
+    return name;
+  }
+
+  private static generateNthRandomCharactersFromString(numberOfRandomElements: number, stringOfCharacters: string): string[] {
+    return (Array.from(Array(numberOfRandomElements)))
+      .map(_currentValue => stringOfCharacters.charAt(Math.random() * stringOfCharacters.length));
+  }
+}
